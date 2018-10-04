d8383bf5f8aaf3759e09de32367e0cbcb67a2ac2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-04 11:17:05 +0200

    Introduce isProbablePrime
    This is a composite of Strategy Int IsPrime. At the moment this only contains
    the smallPrimeDivisor strategy. In the future there will be other
    implementations of probabilistic primes

--- src/Primes.elm
+++ src/Primes.elm
@@ -1,6 +1,7 @@
 module Primes exposing
     ( primes, LazyList
     , take, isPrime
+    , IsPrime, isProbablePrime
     )
 
 {-| Provides a lazy, infinite list of primes.
@@ -117,7 +118,9 @@ primes : LazyList Int
 primes =
     sieve <| integersFrom 2
 
-{-| Determines if the argument is prime. -}
+
+{-| Determines if the argument is prime.
+-}
 isPrime : Int -> Bool
 isPrime n =
     let
@@ -131,3 +134,91 @@ isPrime n =
             List.filter (divisorOf n) candidates
     in
     List.length divisors == 2
+
+
+{-| Type to determine in which category a number falls.
+
+A number is either a `Prime`, a `Composite` number or a `Probable` prime.
+
+-}
+type IsPrime
+    = Prime
+    | Composite Int
+    | Probable
+
+
+type alias Strategy a b =
+    a -> b
+
+
+{-| Determines if the input is a prime, a composite or a probably prime.
+-}
+isProbablePrime : Strategy Int IsPrime
+isProbablePrime n =
+    n
+        |> compoundStrategy
+            [ smallPrimeDivisors
+            ]
+
+
+compoundStrategy : List (Strategy Int IsPrime) -> Strategy Int IsPrime
+compoundStrategy strategies =
+    \n ->
+        let
+            ( _, verdict ) =
+                strategies
+                    |> List.map lift
+                    |> List.foldl (<|) ( n, Probable )
+        in
+        verdict
+
+
+lift : Strategy Int IsPrime -> Strategy ( Int, IsPrime ) ( Int, IsPrime )
+lift strategy =
+    \( n, verdict ) ->
+        case verdict of
+            Prime ->
+                ( n, Prime )
+
+            (Composite _) as composite ->
+                ( n, composite )
+
+            Probable ->
+                ( n, strategy n )
+
+
+smallPrimeDivisors : Strategy Int IsPrime
+smallPrimeDivisors n =
+    let
+        smallPrimes =
+            [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229 ]
+
+        divisorOfN p =
+            modBy p n == 0
+
+        smallDivisor =
+            first divisorOfN smallPrimes
+    in
+    case smallDivisor of
+        Just d ->
+            if d == n then
+                Prime
+            else
+                Composite d
+
+        Nothing ->
+            Probable
+
+
+first : (a -> Bool) -> List a -> Maybe a
+first predicate haystack =
+    case haystack of
+        [] ->
+            Nothing
+
+        x :: xs ->
+            if predicate x then
+                Just x
+
+            else
+                first predicate xs
