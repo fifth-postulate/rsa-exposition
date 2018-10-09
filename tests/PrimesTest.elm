f9d10f2087623ace039b0b04249af9615654c37b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-10-09 18:05:32 +0200

    Parameterize isPrime tests

--- tests/PrimesTest.elm
+++ tests/PrimesTest.elm
@@ -19,33 +19,31 @@ suite =
                     Expect.equal first5Primes [ 2, 3, 5, 7, 11 ]
             ]
         , describe "isPrime"
-            [ describe "should identify primes"
-                [ test "2 is a prime" <|
-                    \_ ->
-                        Expect.true "2 to be prime" (isPrime 2)
-                , test "3 is a prime" <|
-                    \_ ->
-                        Expect.true "3 to be prime" (isPrime 3)
-                , test "5 is a prime" <|
-                    \_ ->
-                        Expect.true "5 to be prime" (isPrime 5)
-                , test "7 is a prime" <|
-                    \_ ->
-                        Expect.true "7 to be prime" (isPrime 7)
-                ]
-            , describe "should identify non-primes"
-                [ test "1 is a not prime" <|
-                    \_ ->
-                        Expect.false "1 to not be prime" (isPrime 1)
-                , test "4 is a not prime" <|
-                    \_ ->
-                        Expect.false "4 to not be prime" (isPrime 4)
-                , test "6 is a not prime" <|
-                    \_ ->
-                        Expect.false "6 to not be prime" (isPrime 6)
-                , test "8 is a not prime" <|
-                    \_ ->
-                        Expect.false "8 to not be prime" (isPrime 8)
-                ]
+            [ describe "should identify primes" <|
+                List.map isItAPrime [ 2, 3, 5, 7 ]
+            , describe "should identify non-primes" <|
+                List.map isItComposite [ 1, 4, 6, 8 ]
             ]
         ]
+
+
+isItAPrime : Int -> Test
+isItAPrime n =
+    let
+        descriptionOfN =
+            String.fromInt n
+    in
+    test (descriptionOfN ++ " is a prime") <|
+        \_ ->
+            Expect.true (descriptionOfN ++ " to be a prime") <| isPrime n
+
+
+isItComposite : Int -> Test
+isItComposite n =
+    let
+        descriptionOfN =
+            String.fromInt n
+    in
+    test (descriptionOfN ++ " is a composite") <|
+        \_ ->
+            Expect.false (descriptionOfN ++ " to be composite") <| isPrime n
